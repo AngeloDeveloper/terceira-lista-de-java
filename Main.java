801ddc1f945747735322c05f9ef7ebd5789e0c57
Angelo Gabriel Silva Ribeiro <angelogabrielsilvaribeiro@gmail.com>
2018-03-28 19:21:08 -0300

    q1
    primeira questão gg é nos

---
+++ Main.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Scanner entrada= new Scanner(System.in);
+
+
+        int n= entrada.nextInt();
+
+
+        boolean vdd = true;
+
+
+        for (int i = 2; i < n; i++)
+        {
+
+
+
+            if (vdd) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                }
+            } else {
+                if (i % 2 != 0) {
+                    System.out.print(i + " ");
+                }
+            }
+
+            if (i % 10 == 0)
+            {
+                vdd = !vdd;
+            }
+
+        }
+
+    }
+}
